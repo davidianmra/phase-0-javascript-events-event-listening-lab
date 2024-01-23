f9c17dec3d9f115b6626840b2bc3214768692a0d
David Rashid <davidmra@umich.edu>
2024-01-23 15:34:33 -0500

    All done

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+
+
+function alertMe(){
+    alert('I was clicked');
+}
+const input2 = document.querySelector('#button');
+input2.addEventListener('click', alertMe);
 }
